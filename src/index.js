b2fcda055c97a2034f6dc708a81be7196807a679
Kuldeep Kumar <upsainian3330@gmail.com>
2024-05-15 23:56:50 +0530

    app fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import { connectDB } from "./db/index.js"
+import {app} from './app.js'
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on PORT:${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log(`MONGODB CONNECTIO FAILED: ${err}`);
+})
